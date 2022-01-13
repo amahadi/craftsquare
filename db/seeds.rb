e91d8162dce4a29a32c15ef360a27b0493a17bdd
alvi2496 <alvi.utsab@gmail.com>
2022-01-13 11:13:30 -0800

    add test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-merchant = Merchant.find_by(email: "merchant@example.com")
+merchant = Merchant.find_by(email: "seededmerchant@example.com")
 
 unless merchant.present?
     merchant = Merchant.create(
-        email: "merchant@example.com",
+        email: "seededmerchant@example.com",
         password: "password",
         password_confirmation: "password"
     )
 end
 
-customer = Customer.find_by(email: "customer@example.com")
+customer = Customer.find_by(email: "seededcustomer@example.com")
 
 unless customer.present?
     customer = Customer.create(
-        email: "customer@example.com",
+        email: "seededcustomer@example.com",
         password: "password",
         password_confirmation: "password",
         first_name: "Example",
--- test/models/merchant_test.rb
+++ test/models/merchant_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class MerchantTest < ActiveSupport::TestCase
+
   test "Email must be provided" do
     merchant = Merchant.new
     assert_not merchant.save
@@ -11,21 +12,42 @@ class MerchantTest < ActiveSupport::TestCase
     assert_not merchant.save
   end
 
+  test "Merchant should be created if email and password in provided" do
+    merchant = Merchant.new(
+      email: 'merchant@example.com',
+      password: 'password',
+      password_confirmation: 'password'
+    )
+    assert merchant.save
+  end
+
   test "First name has valid characters" do
-    merchant = Merchant.first
+    merchant = Merchant.find_by(email: 'dummy_merchant@example.com')
     merchant.first_name = "Merchant!@#$567"
-    assert_not merchant.save
+    begin
+      merchant.save
+    rescue => exception
+      assert merchant.errors[:merchant].present?
+    end
   end
 
   test "Last name has valid characters" do
-    merchant = Merchant.first
+    merchant = Merchant.find_by(email: 'dummy_merchant@example.com')
     merchant.last_name = "Merchant!@#$567"
-    assert_not merchant.save
+    begin
+      merchant.save
+    rescue => exception
+      assert merchant.errors[:merchant].present?
+    end
   end
 
   test "Should be above the age of 16" do
-    merchant = Merchant.first
+    merchant = Merchant.find_by(email: 'dummy_merchant@example.com')
     merchant.date_of_birth = "2020-12-12"
-    assert_not merchant.save
+    begin
+      merchant.save
+    rescue => exception
+      assert merchant.errors[:merchant].present?
+    end
   end
 end
